3cf62d4e72fe86efb7051f6c776ef8965701b9ea
Raul Gonzalez <rauls.gh24@gmail.com>
2022-11-14 14:08:20 -0600

    Actualizar README y codigo

--- exercises/agenda/README.md
+++ exercises/agenda/README.md
@@ -1,3 +1,41 @@
 # Agenda
 
 ## Requisitos
+
+- Crear una clase `Usuario` que contenga lo siguiente
+  - Propiedades
+    - nombres
+    - apellidos
+    - telefono
+  - Metodos:
+    - traerNombres
+    - traerApellidos
+    - traerTelefono
+- Funcion de `agregar`
+  - Al presionar al boton de `Agregar` se debera crear un nuevo objeto de `Usuario`
+  - Agregar al objecto el id generado
+  - Verificar que ningun campo este vacio
+  - Actualizar al arreglo de `agenda`
+- Funcion `eliminar`
+  - Buscar y eliminar el elemento el cual se le presiono el icono de eliminar
+- Funcion `editar`
+  - Rellenar los inputs con el elemento presionado
+- Funcion de `actualizar`
+  - Presionar el boton de `Actulizar` y actualizar el objeto del arreglo de `agenda`
+  - Verificar que ningun campo este vacio
+  - Actualizar tabla con los nuevos datos
+
+Las siguientes funciones les serviran como auxiliares:
+
+- `traerDatosTr`
+  - Al presionar el icono de `editar/eliminar` esta se traera los datos de la fila
+- `traerDatosInputs`
+  - Traera los datos que actualmente estan en los inputs
+- `limpiarInputs`
+  - Limipiara todo el formulario
+- `rellenarInputs`
+  - Llenara los inputs con la informacion proporcionada
+- `popularTabla`
+  - Llenara la tabla con el arreglos que se le proporciona, recordar que cada elemento del arreglo debe ser un `User`
+- `generarId`
+  - Genera un id aleatorio
--- exercises/agenda/index.js
+++ exercises/agenda/index.js
@@ -1,31 +1,36 @@
-const actualOperacion = null;
-
 class Usuario {}
 
-const agenda = [];
+let agenda = [];
 
 function agregar() {
   // ¡¡¡ NO TOCAR !!!
   // Generar ID
   const id = generarId();
+  // Traer datos de los inputs
+  const { nombres, apellidos, telefono } = traerDatosInputs();
   // !!!!!!!!!!!!!!!
 }
 
 function eliminar(evento) {
   // ¡¡¡ NO TOCAR !!!
   // Traer los datos de la fila
-  const { id, nombre, apellido, telefono } = traerDatos(evento);
+  const { id } = traerDatosTr(evento);
   // !!!!!!!!!!!!!!!
 }
 
 function editar(evento) {
   // ¡¡¡ NO TOCAR !!!
   // Traer los datos de la fila
-  const { id, nombre, apellido, telefono } = traerDatos(evento);
+  const { id, nombres, apellidos, telefono } = traerDatosTr(evento);
   // !!!!!!!!!!!!!!!
 }
 
-function realizarOperacion() {}
+function actualizar(evento) {
+  // ¡¡¡ NO TOCAR !!!
+  // Traer los datos de los inputs
+  const { id, nombres, apellidos, telefono } = traerDatosInputs(evento);
+  // !!!!!!!!!!!!!!!
+}
 
 /*
   ¡¡¡ NO TOCAR !!!
@@ -33,48 +38,67 @@ function realizarOperacion() {}
   !!!!!!!!!!!!!!!!
 */
 
-function traerDatos(icon) {
+function traerDatosTr(icon) {
   const tr = icon.closest('tr');
-  const [id, nombre, apellido, telefono] = Array.from(tr.children)
+  const [id, nombres, apellidos, telefono] = Array.from(tr.children)
     .slice(0, -1)
     .map((a) => a.innerText);
-  return { id, nombre, apellido, telefono };
+  return { id, nombres, apellidos, telefono };
+}
+
+function traerDatosInputs() {
+  const formData = new FormData(document.getElementById('formulario'));
+  return {
+    id: formData.get('id'),
+    nombres: formData.get('nombres'),
+    apellidos: formData.get('apellidos'),
+    telefono: formData.get('telefono'),
+  };
 }
 
 function limpiarInputs() {
   document.getElementById('formulario').reset();
 }
 
-function rellenarInputs(nombre, apellido, telefono) {
-  document.querySelector('#formulario > div:nth-child(1) > input').value = nombre;
-  document.querySelector('#formulario > div:nth-child(2) > input').value = apellido;
-  document.querySelector('#formulario > div:nth-child(3) > input').value = telefono;
+function rellenarInputs(id, nombres, apellidos, telefono) {
+  document.querySelector('#formulario > input').value = id;
+  document.querySelector('#formulario > div:nth-child(2) > input').value = nombres;
+  document.querySelector('#formulario > div:nth-child(3) > input').value = apellidos;
+  document.querySelector('#formulario > div:nth-child(4) > input').value = telefono;
 }
 
 function popularTabla(datos) {
-  const tbodyNuevo = document.createElement('tbody');
+  const tbody = document.getElementById('tabla-contenido');
+  tbody.innerHTML = '';
+
   datos
     .map((d) => crearTr(d))
     .forEach((tr) => {
-      tbodyNuevo.appendChild(tr);
+      tbody.appendChild(tr);
     });
-  const tbodyAnterior = document.getElementById('tabla-contenido');
-  tbodyAnterior.parentNode.replaceChildren(tbodyNuevo, tbodyAnterior);
 }
 
 function crearTr(dato) {
-  const tr = document.createElement('tr');
-  tr.appendChild((document.createElement('td').innerText = dato.nombre));
-  tr.appendChild((document.createElement('td').innerText = dato.apellido));
-  tr.appendChild((document.createElement('td').innerText = dato.telefono));
-  return tr;
+  const tdHTML = `
+    <td>${dato.id}</td>
+    <td>${dato.traerNombres()}</td>
+    <td>${dato.traerApellidos()}</td>
+    <td>${dato.traerTelefono()}</td>
+    <td>
+      <i class="bi bi-pencil-square" onclick="editar(this)"></i>
+      <i class="bi bi-trash3" onclick="eliminar(this)"></i>
+    </td>
+  `;
+  const td = document.createElement('tr');
+  td.innerHTML = tdHTML;
+  return td;
 }
 
 function generarId() {
   const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let resultado = ' ';
+  let resultado = '';
   for (let i = 0; i < 5; i++) {
-    result += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    resultado += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
   }
   return resultado;
 }
